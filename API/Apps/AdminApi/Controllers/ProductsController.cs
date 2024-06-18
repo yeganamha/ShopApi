e7cd5ce5d5588442feb6b8a9f5f27bb7d97662f0
Yegana Hasanova <yeganamha@code.edu.az>
2024-06-18 04:55:40 -0700

    Dto intro

---
+++ API/Apps/AdminApi/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Apps.AdminApi.Controllers
+{
+    [Route("admin/api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopDbContext _context;
+
+        public ProductsController(ShopDbContext context) 
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var data = _context.Products.ToList();
+            return Ok(data);
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var data = _context.Products.Find(id);
+            if(data==null)
+            return NotFound();
+            return Ok(data);
+        }
+
+    }
+}
